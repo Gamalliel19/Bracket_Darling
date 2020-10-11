8357a336df7efb66357127ccc19a523bf66d14ad
Gamalliel Sharon <you@example.com>
2020-10-11 17:55:25 +0700

    menambahkan media query

---
+++ script/app.js
@@ -0,0 +1,13 @@
+const setting = document.querySelector('.setting-btns');
+const backdrop = document.querySelector('.backdrop-popup');
+const popup = document.querySelector('.popup');
+
+setting.addEventListener('click', ()=>{
+    backdrop.classList.add('active');
+    popup.classList.add('active');
+})
+
+backdrop.addEventListener('click', () => {
+    backdrop.classList.remove('active');
+    popup.classList.remove('active')
+})
